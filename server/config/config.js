af5645e4d621a44bd85e37a055b8b05cc1425c09
Manuel J Saavedra Severiche <saavedramanuel100@gmail.com>
2020-05-31 20:47:44 -0400

    solucionando error de conexion

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ process.env.PORT = process.env.PORT || 3007;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Base de datos
-let url = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe-nodejs' : 'mongodb+srv://manuelnode:5DeGhj8zh@2$SJ$@cluster0-6iuza.mongodb.net/test';
+let url = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe-nodejs' : 'mongodb+srv://manuelnode:5DeGhj8zh@2$SJ$@cluster0-6iuza.mongodb.net/cafe-nodejs';
 
 process.env.URL_DATABASE = url;
 
