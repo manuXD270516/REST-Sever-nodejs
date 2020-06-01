dcc25846c3db918fcb1b0b54120aa88b22dc94d0
Manuel J Saavedra Severiche <saavedramanuel100@gmail.com>
2020-05-31 20:54:49 -0400

    solucionando problema de conexion mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
 process.env.PORT = process.env.PORT || 3007;
 
 // Entorno
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'prod';
 
 // Base de datos
 let url = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe-nodejs' : 'mongodb+srv://manuelnode:5DeGhj8zh@2$SJ$@cluster0-6iuza.mongodb.net/cafe-nodejs';
