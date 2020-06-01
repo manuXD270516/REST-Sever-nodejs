ea66613d1eba0999cc0d1630e9e811c9531215bf
Manuel J Saavedra Severiche <saavedramanuel100@gmail.com>
2020-05-31 21:44:46 -0400

    modificando las variables de entorno de la conexion de base de datos

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Base de datos
 console.log(process.env.NODE_ENV);
-let url = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe-nodejs' : 'mongodb+srv://manuelnode:5DeGhj8zh@2$SJ$@cluster0-6iuza.mongodb.net/cafe-nodejs?retryWrites=true&w=majority';
+let url = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe-nodejs' : process.env.MONGO_URI_DATABASE;
 
 process.env.URL_DATABASE = url;
 
